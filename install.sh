9a69db47dfcffa5a1c5f1c9da095f3a773f8d979
buggysolid <buggysolid@bugbounty.club>
2022-06-28 20:49:47 +0100

    Remove script run.log.
    I have no idea WHY but pipping out to tee and appending to a log file to
    capture any failed runs of the scripts was causing the install script
    to bail out after configuring Unbound.
    
    Any of the commands from unbound-anchor onwards did not execute. I will
    need to look into why but it is confirmed to be fixed now that the the
    pipe is removed.

--- install.sh
+++ install.sh
@@ -11,33 +11,33 @@ fi
 
 rm -f run.log && touch run.log
 echo "Updating system."
-apt-get update | tee -a run.log
+apt-get update
 echo "Installing dependencies."
-apt-get install -y build-essential libssl-dev libexpat1-dev bison flex libevent-dev libsodium-dev libprotobuf-c-dev wget tar make | tee -a run.log
+apt-get install -y build-essential libssl-dev libexpat1-dev bison flex libevent-dev libsodium-dev libprotobuf-c-dev wget tar make
 echo "Grabbing the latest version of Unbound."
-wget 'https://nlnetlabs.nl/downloads/unbound/unbound-latest.tar.gz' | tee -a run.log
+wget 'https://nlnetlabs.nl/downloads/unbound/unbound-latest.tar.gz'
 echo "Extracting Unbound."
-tar xzf unbound-latest.tar.gz | tee -a run.log
+tar xzf unbound-latest.tar.gz
 echo "Configuring Unbound."
 cd "$(ls -d ./*unbound*/)"
-./configure --with-pthreads --with-libevent --with-protobuf-c --with-libsodium | tee -a run.log
+./configure --with-pthreads --with-libevent --with-protobuf-c --with-libsodium
 echo "Building Unbound from source."
-make | tee -a run.log
-make install | tee -a run.log
-useradd unbound | tee -a run.log
-chown -R unbound /usr/local/etc/unbound | tee -a run.log
-wget 'https://raw.githubusercontent.com/buggysolid/unbound-config/main/unbound.conf' -O /usr/local/etc/unbound/unbound.conf | tee -a run.log
-ldconfig | tee -a run.log
-unbound-anchor | tee -a run.log
-unbound-control-setup | tee -a run.log
-wget 'https://raw.githubusercontent.com/buggysolid/unbound-config/main/unbound.service' -O /etc/systemd/system/unbound.service | tee -a run.log
-chmod 755 /etc/systemd/system/unbound.service | tee -a run.log
-wget 'https://raw.githubusercontent.com/buggysolid/unbound-config/main/resolv.conf' -O "$HOME/resolv.conf" | tee -a run.log
-mv "$HOME/resolv.conf" /etc/resolv.conf | tee -a run.log
-chmod 744 /etc/resolv.conf | tee -a run.log
-systemctl stop systemd-resolved | tee -a run.log
-systemctl disable systemd-resolved | tee -a run.log
-systemctl enable unbound | tee -a run.log
-systemctl restart unbound | tee -a run.log
-systemctl status unbound | tee -a run.log
+make
+make install
+useradd unbound
+chown -R unbound /usr/local/etc/unbound
+wget 'https://raw.githubusercontent.com/buggysolid/unbound-config/main/unbound.conf' -O /usr/local/etc/unbound/unbound.conf
+ldconfig
+unbound-anchor
+unbound-control-setup
+wget 'https://raw.githubusercontent.com/buggysolid/unbound-config/main/unbound.service' -O /etc/systemd/system/unbound.service
+chmod 755 /etc/systemd/system/unbound.service
+wget 'https://raw.githubusercontent.com/buggysolid/unbound-config/main/resolv.conf' -O "$HOME/resolv.conf"
+mv "$HOME/resolv.conf" /etc/resolv.conf
+chmod 744 /etc/resolv.conf
+systemctl stop systemd-resolved
+systemctl disable systemd-resolved
+systemctl enable unbound
+systemctl restart unbound
+systemctl status unbound
 echo "Unbound installed."
