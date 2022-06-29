04cf6fc95d4eceef3dd7783061b808e949407be8
buggysolid <buggysolid@bugbounty.club>
2022-06-29 16:15:23 +0100

    Revert "Remove script run.log."
    This reverts commit 9a69db47dfcffa5a1c5f1c9da095f3a773f8d979.
    
    It turns out this was not causing any issues. I noticed the script was
    exiting either before or after unbound-anchor. I read the man page of
    unbound-anchor and it turns out it exits with a return code of 1 in a
    successful state.
    
    EXIT CODE
           This tool exits with value 1 if the root anchor was updated using the certificate or if the builtin root-anchor was used.  It exits with code 0 if no update was necessary, if the update was possi‐
           ble with RFC5011 tracking, or if an error occurred.
    
           You can check the exit value in this manner:
                unbound-anchor -a "root.key" || logger "Please check root.key"
           Or something more suitable for your operational environment.
    
    I set the errexit option at the top of the script so this is what was
    triggering it. The script will need to be adjusted to account for this.

--- install.sh
+++ install.sh
@@ -11,33 +11,33 @@ fi
 
 rm -f run.log && touch run.log
 echo "Updating system."
-apt-get update
+apt-get update | tee -a run.log
 echo "Installing dependencies."
-apt-get install -y build-essential libssl-dev libexpat1-dev bison flex libevent-dev libsodium-dev libprotobuf-c-dev wget tar make
+apt-get install -y build-essential libssl-dev libexpat1-dev bison flex libevent-dev libsodium-dev libprotobuf-c-dev wget tar make | tee -a run.log
 echo "Grabbing the latest version of Unbound."
-wget 'https://nlnetlabs.nl/downloads/unbound/unbound-latest.tar.gz'
+wget 'https://nlnetlabs.nl/downloads/unbound/unbound-latest.tar.gz' | tee -a run.log
 echo "Extracting Unbound."
-tar xzf unbound-latest.tar.gz
+tar xzf unbound-latest.tar.gz | tee -a run.log
 echo "Configuring Unbound."
 cd "$(ls -d ./*unbound*/)"
-./configure --with-pthreads --with-libevent --with-protobuf-c --with-libsodium
+./configure --with-pthreads --with-libevent --with-protobuf-c --with-libsodium | tee -a run.log
 echo "Building Unbound from source."
-make
-make install
-useradd unbound
-chown -R unbound /usr/local/etc/unbound
-wget 'https://raw.githubusercontent.com/buggysolid/unbound-config/main/unbound.conf' -O /usr/local/etc/unbound/unbound.conf
-ldconfig
-unbound-anchor
-unbound-control-setup
-wget 'https://raw.githubusercontent.com/buggysolid/unbound-config/main/unbound.service' -O /etc/systemd/system/unbound.service
-chmod 755 /etc/systemd/system/unbound.service
-wget 'https://raw.githubusercontent.com/buggysolid/unbound-config/main/resolv.conf' -O "$HOME/resolv.conf"
-mv "$HOME/resolv.conf" /etc/resolv.conf
-chmod 744 /etc/resolv.conf
-systemctl stop systemd-resolved
-systemctl disable systemd-resolved
-systemctl enable unbound
-systemctl restart unbound
-systemctl status unbound
+make | tee -a run.log
+make install | tee -a run.log
+useradd unbound | tee -a run.log
+chown -R unbound /usr/local/etc/unbound | tee -a run.log
+wget 'https://raw.githubusercontent.com/buggysolid/unbound-config/main/unbound.conf' -O /usr/local/etc/unbound/unbound.conf | tee -a run.log
+ldconfig | tee -a run.log
+unbound-anchor | tee -a run.log
+unbound-control-setup | tee -a run.log
+wget 'https://raw.githubusercontent.com/buggysolid/unbound-config/main/unbound.service' -O /etc/systemd/system/unbound.service | tee -a run.log
+chmod 755 /etc/systemd/system/unbound.service | tee -a run.log
+wget 'https://raw.githubusercontent.com/buggysolid/unbound-config/main/resolv.conf' -O "$HOME/resolv.conf" | tee -a run.log
+mv "$HOME/resolv.conf" /etc/resolv.conf | tee -a run.log
+chmod 744 /etc/resolv.conf | tee -a run.log
+systemctl stop systemd-resolved | tee -a run.log
+systemctl disable systemd-resolved | tee -a run.log
+systemctl enable unbound | tee -a run.log
+systemctl restart unbound | tee -a run.log
+systemctl status unbound | tee -a run.log
 echo "Unbound installed."
