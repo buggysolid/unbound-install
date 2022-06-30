119b23a2c629bd110c7d27525bf560a587e9641f
Ciaran <106989304+buggysolid@users.noreply.github.com>
2022-06-30 14:11:03 +0100

    Split install files to each package manager/distro.
    I commonly use a mix of distros so I want these install scripts to work
    for deb based distros and RPM based distros.
    
    The common thread of using SystemD still holds true here.

---
+++ install-yum.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+# Install the latest version of Unbound and start it as a SystemD service.
+
+set -o errexit
+set -o pipefail
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run the script as root or invoke via sudo."
+  exit
+fi
+
+rm -f run.log && touch run.log
+echo "Updating system."
+yum update -y | tee -a run.log
+echo "Installing dependencies."
+yum install -y epel-release
+yum update -y
+yum install -y openssl-devel expat-devel bison flex libevent-devel libsodium-devel protobuf wget tar make gcc | tee -a run.log
+echo "Grabbing the latest version of Unbound."
+wget 'https://nlnetlabs.nl/downloads/unbound/unbound-latest.tar.gz' | tee -a run.log
+echo "Extracting Unbound."
+tar xzf unbound-latest.tar.gz | tee -a run.log
+echo "Configuring Unbound."
+cd "$(ls -d ./*unbound*/)"
+./configure --with-pthreads --with-libevent --with-protobuf-c --with-libsodium | tee -a run.log
+echo "Building Unbound from source."
+make | tee -a run.log
+make install | tee -a run.log
+useradd unbound | tee -a run.log
+chown -R unbound /usr/local/etc/unbound | tee -a run.log
+wget 'https://raw.githubusercontent.com/buggysolid/unbound-config/main/unbound.conf' -O /usr/local/etc/unbound/unbound.conf | tee -a run.log
+ldconfig | tee -a run.log
+unbound-anchor || echo "Unbound-anchor may have failed to update the root.key used to verify DNSSEC signatures." | tee -a run.log
+unbound-control-setup | tee -a run.log
+wget 'https://raw.githubusercontent.com/buggysolid/unbound-config/main/unbound.service' -O /etc/systemd/system/unbound.service | tee -a run.log
+chmod 755 /etc/systemd/system/unbound.service | tee -a run.log
+wget 'https://raw.githubusercontent.com/buggysolid/unbound-config/main/resolv.conf' -O "$HOME/resolv.conf" | tee -a run.log
+mv "$HOME/resolv.conf" /etc/resolv.conf | tee -a run.log
+chmod 744 /etc/resolv.conf | tee -a run.log
+systemctl enable unbound | tee -a run.log
+systemctl restart unbound | tee -a run.log
+systemctl status unbound | tee -a run.log
+echo "Unbound installed."
